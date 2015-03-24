bdcbfbceecfbb6e4f1c333768d07170090d2af12
Patrik Jira <patrikjira@gmail.com>
2015-03-24 12:06:37 +0100

    feat: google analytics

--- config/environment.js
+++ config/environment.js
@@ -28,6 +28,7 @@ module.exports = function(environment) {
   };
 
   if (environment === 'development') {
+    ENV.googleAnalytics = { webPropertyId: 'UA-60379606-1' };
     // ENV.APP.LOG_RESOLVER = true;
     // ENV.APP.LOG_ACTIVE_GENERATION = true;
     // ENV.APP.LOG_TRANSITIONS = true;
