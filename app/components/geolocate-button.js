a9afa46f74b6b61181c654eaa2674657700ae141
Josef Šimánek <josef.simanek@gmail.com>
2015-04-13 00:34:41 +0200

    Geolocation.

---
+++ app/components/geolocate-button.js
@@ -0,0 +1,27 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  action: 'centerMap',
+  classNames: ['btn', 'btn-default'],
+  tagName: 'button',
+  classNameBindings: ['hidden'],
+  hidden: true,
+  latitude: null,
+  longitude: null,
+  didInsertElement: function() {
+    if(navigator && navigator.geolocation) {
+      this.set('hidden', false);
+    }
+  },
+  click: function() {
+    var self = this;
+    if(navigator && navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(function(position) {
+        self.sendAction('action', position.coords.latitude, position.coords.longitude);
+      }, function() {
+        console.info('Geolocation was denied in the browser.');
+      });
+    };
+    return false;
+  },
+});
