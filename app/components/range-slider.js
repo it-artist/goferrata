a9e4d4f6267fea80c39add76e790adb6db29977c
Josef Šimánek <josef.simanek@gmail.com>
2015-04-26 17:16:37 +0200

    Use real JS syntax.

--- app/components/range-slider.js
+++ app/components/range-slider.js
@@ -9,19 +9,19 @@ export default RangeSlider.extend({
     this._super();
     let pips_settings = { mode: 'range' }
 
-    if (this.kind == "height") {
+    if (this.kind === "height") {
       pips_settings.format = {
         to: function ( value ) {
           return value + 'm';
         }
-      }
+      };
     }
-    else if (this.kind == "duration") {
+    else if (this.kind === "duration") {
       pips_settings.format = {
         to: function ( value ) {
           return parseInt(value / 60) + 'h';
         }
-      }
+      };
       pips_settings.density = 6;
     }
 
