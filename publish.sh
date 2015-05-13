c1226dcae54a2127b11fe32f18c96d7752de2489
Patrik Jira <patrikjira@gmail.com>
2015-05-13 18:09:47 +0200

    chore: new production location

--- publish.sh
+++ publish.sh
@@ -1,10 +1,5 @@
 set -e
 
 ember build --e production
-cd dist
-git init
-git checkout -b gh-pages
-git remote add origin git@github.com:papricek/goferrata.git
-git add -A
-git commit -m "Deploy"
-git push -f origin gh-pages
+
+rsync -avz --delete dist/ goferrata@r01.avatech.cz:/home/goferrata/web/current/public
